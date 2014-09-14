cf8327719a9f64ee7f4c65b6dccd8036680ff7c6
Katrina Owen <katrina.owen@gmail.com>
2014-09-14 13:09:14 -0700

    Shameless Green - 01
    Get the very first test passing

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,3 +1,10 @@
 class Bottles
 
+  def verse(_)
+    "99 bottles of milk on the wall, " +
+    "99 bottles of milk.\n" +
+    "Take one down and pass it around, " +
+    "98 bottles of milk on the wall.\n"
+  end
+
 end
