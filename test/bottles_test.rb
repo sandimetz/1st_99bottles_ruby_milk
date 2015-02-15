9eaad566128b25575b16668d63ade42b54cd6ed7
Katrina Owen <katrina.owen@gmail.com>
2015-02-14 18:22:38 -0800

    TDD: test for verses 2, 1, 0

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -60,4 +60,22 @@ class BottlesTest < Minitest::Test
       "97 bottles of milk on the wall.\n"
     assert_equal expected, Bottles.new.verses(99, 98)
   end
+
+  def test_a_few_verses
+    expected = "2 bottles of milk on the wall, " +
+      "2 bottles of milk.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of milk on the wall.\n" +
+      "\n" +
+      "1 bottle of milk on the wall, " +
+      "1 bottle of milk.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of milk on the wall.\n" +
+      "\n" +
+      "No more bottles of milk on the wall, " +
+      "no more bottles of milk.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of milk on the wall.\n"
+    assert_equal expected, Bottles.new.verses(2, 0)
+  end
 end
