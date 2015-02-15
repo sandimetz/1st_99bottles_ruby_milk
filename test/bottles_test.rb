8ce2690041846941642110646894306cccc8246e
Katrina Owen <katrina.owen@gmail.com>
2015-02-14 18:05:12 -0800

    TDD: test for verse 1

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -31,4 +31,12 @@ class BottlesTest < Minitest::Test
       "1 bottle of milk on the wall.\n"
     assert_equal expected, Bottles.new.verse(2)
   end
+
+  def test_verse_1
+    expected = "1 bottle of milk on the wall, " +
+      "1 bottle of milk.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of milk on the wall.\n"
+    assert_equal expected, Bottles.new.verse(1)
+  end
 end
