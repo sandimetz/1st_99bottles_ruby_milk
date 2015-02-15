28618e16d414e64801c9211f42be1ee9f80e8233
Katrina Owen <katrina.owen@gmail.com>
2015-02-14 18:34:31 -0800

    TDD: FIXME - missing test coverage

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,6 +3,7 @@
     # ...
       # ...
 class Bottles
+  # FIXME: no test coverage for 4..98
   def verse(number)
     "#{number} bottles of milk on the wall, " +
     "#{number} bottles of milk.\n" +
