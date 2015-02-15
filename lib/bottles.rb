13b30e04b932b87f624073d4744aba3e3703619d
Katrina Owen <katrina.owen@gmail.com>
2015-02-14 17:49:09 -0800

    TDD: code for verses 99 and 3

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,10 +3,17 @@
     # ...
       # ...
 class Bottles
-  def verse(_)
-    "99 bottles of milk on the wall, " +
-    "99 bottles of milk.\n" +
-    "Take one down and pass it around, " +
-    "98 bottles of milk on the wall.\n"
+  def verse(number)
+    if number == 99
+      "99 bottles of milk on the wall, " +
+      "99 bottles of milk.\n" +
+      "Take one down and pass it around, " +
+      "98 bottles of milk on the wall.\n"
+    else
+      "3 bottles of milk on the wall, " +
+      "3 bottles of milk.\n" +
+      "Take one down and pass it around, " +
+      "2 bottles of milk on the wall.\n"
+    end
   end
 end
