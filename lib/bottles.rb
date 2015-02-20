sandimetz/1st_99bottles_ruby

TDD: code for verse 1

lib/bottles.rb
@@ -5,7 +5,13 @@
 class Bottles
   # FIXME: no test coverage for 4..98
   def verse(number)
-    if number == 2
+    case number
+    when 1
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
+    when 2
       "2 bottles of beer on the wall, " +
       "2 bottles of beer.\n" +
       "Take one down and pass it around, " +

sandimetz/1st_99bottles_ruby_milk

lib/bottles.rb
@@ -5,7 +5,13 @@
 class Bottles
   # FIXME: no test coverage for 4..98
   def verse(number)
-    if number == 2
+    case number
+    when 1
+      "1 bottle of milk on the wall, " +
+      "1 bottle of milk.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of milk on the wall.\n"
+    when 2
       "2 bottles of milk on the wall, " +
       "2 bottles of milk.\n" +
       "Take one down and pass it around, " +