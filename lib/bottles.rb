sandimetz/1st_99bottles_ruby

TDD: code for verse 99

lib/bottles.rb
@@ -1,0 +1,12 @@
+# ...
+  # ...
+    # ...
+      # ...
+class Bottles
+  def verse(_)
+    "99 bottles of beer on the wall, " +
+    "99 bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "98 bottles of beer on the wall.\n"
+  end
+end

sandimetz/1st_99bottles_ruby_milk

lib/bottles.rb
@@ -1,0 +1,12 @@
+# ...
+  # ...
+    # ...
+      # ...
+class Bottles
+  def verse(_)
+    "99 bottles of milk on the wall, " +
+    "99 bottles of milk.\n" +
+    "Take one down and pass it around, " +
+    "98 bottles of milk on the wall.\n"
+  end
+end