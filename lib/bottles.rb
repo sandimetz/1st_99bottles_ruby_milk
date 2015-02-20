sandimetz/1st_99bottles_ruby

TDD: code for verse 0

lib/bottles.rb
@@ -6,6 +6,11 @@ class Bottles
   # FIXME: no test coverage for 4..98
   def verse(number)
     case number
+    when 0
+      "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
     when 1
       "1 bottle of beer on the wall, " +
       "1 bottle of beer.\n" +

sandimetz/1st_99bottles_ruby_milk

lib/bottles.rb
@@ -6,6 +6,11 @@ class Bottles
   # FIXME: no test coverage for 4..98
   def verse(number)
     case number
+    when 0
+      "No more bottles of milk on the wall, " +
+      "no more bottles of milk.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of milk on the wall.\n"
     when 1
       "1 bottle of milk on the wall, " +
       "1 bottle of milk.\n" +