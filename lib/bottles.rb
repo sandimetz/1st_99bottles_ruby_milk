sandimetz/1st_99bottles_ruby

TDD: code for verses 3-99

lib/bottles.rb
@@ -4,16 +4,9 @@
       # ...
 class Bottles
   def verse(number)
-    if number == 99
-      "99 bottles of beer on the wall, " +
-      "99 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "98 bottles of beer on the wall.\n"
-    else
-      "3 bottles of beer on the wall, " +
-      "3 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "2 bottles of beer on the wall.\n"
-    end
+    "#{number} bottles of beer on the wall, " +
+    "#{number} bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "#{number-1} bottles of beer on the wall.\n"
   end
 end

sandimetz/1st_99bottles_ruby_milk

lib/bottles.rb
@@ -4,16 +4,9 @@
       # ...
 class Bottles
   def verse(number)
-    if number == 99
-      "99 bottles of milk on the wall, " +
-      "99 bottles of milk.\n" +
-      "Take one down and pass it around, " +
-      "98 bottles of milk on the wall.\n"
-    else
-      "3 bottles of milk on the wall, " +
-      "3 bottles of milk.\n" +
-      "Take one down and pass it around, " +
-      "2 bottles of milk on the wall.\n"
-    end
+    "#{number} bottles of milk on the wall, " +
+    "#{number} bottles of milk.\n" +
+    "Take one down and pass it around, " +
+    "#{number-1} bottles of milk on the wall.\n"
   end
 end