sandimetz/1st_99bottles_ruby

TDD: code for verses 99 and 98, literal strings

lib/bottles.rb
@@ -3,6 +3,18 @@
     # ...
       # ...
 class Bottles
+  def verses(_, _)
+    "99 bottles of beer on the wall, " +
+    "99 bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "98 bottles of beer on the wall.\n" +
+    "\n" +
+    "98 bottles of beer on the wall, " +
+    "98 bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "97 bottles of beer on the wall.\n"
+  end
+
   # FIXME: no test coverage for 4..98
   def verse(number)
     case number

sandimetz/1st_99bottles_ruby_milk

lib/bottles.rb
@@ -3,6 +3,18 @@
     # ...
       # ...
 class Bottles
+  def verses(_, _)
+    "99 bottles of milk on the wall, " +
+    "99 bottles of milk.\n" +
+    "Take one down and pass it around, " +
+    "98 bottles of milk on the wall.\n" +
+    "\n" +
+    "98 bottles of milk on the wall, " +
+    "98 bottles of milk.\n" +
+    "Take one down and pass it around, " +
+    "97 bottles of milk on the wall.\n"
+  end
+
   # FIXME: no test coverage for 4..98
   def verse(number)
     case number