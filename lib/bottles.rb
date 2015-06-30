63f4c728d490b1fd2f6a3d3cd02ce5c1a1b8d6e3
Katrina Owen <katrina.owen@gmail.com>
2015-06-30 08:59:31 -0600

    Coalesce: action - send the other message

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -18,7 +18,7 @@ class Bottles
     when 0
       "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " +
       "#{quantity(number)} #{container(number)} of milk.\n" +
-      "Go to the store and buy some more, " +
+      "#{action(number)}, " +
       "99 bottles of milk on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " +
