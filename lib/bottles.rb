5b03da82a86645420f6e3a31af63e07125c9d307
Katrina Owen <katrina.owen@gmail.com>
2015-06-30 08:52:31 -0600

    Coalesce: use container in line 2 of 'when 0'

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -17,7 +17,7 @@ class Bottles
     case number
     when 0
       "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " +
-      "#{quantity(number)} bottles of milk.\n" +
+      "#{quantity(number)} #{container(number)} of milk.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of milk on the wall.\n"
     else
