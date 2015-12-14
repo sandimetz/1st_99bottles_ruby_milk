a50d244ea7eed557c2944973c76219744217298f
Sandi Metz <sandi@sandimetz.com>
2015-12-14 09:02:46 -0500

    extend verse conditional with six-packs

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -7,8 +7,6 @@ class Bottles
     verses(99, 0)
   end
 
-  private
-
   def verses(starting, ending)
     starting.downto(ending).collect {|i| verse(i)}.join("\n")
   end
@@ -30,6 +28,16 @@ class Bottles
       "2 bottles of milk.\n" +
       "Take one down and pass it around, " +
       "1 bottle of milk on the wall.\n"
+    when 6
+      "1 six-pack of milk on the wall, " +
+      "1 six-pack of milk.\n" +
+      "Take one down and pass it around, " +
+      "5 bottles of milk on the wall.\n"
+    when 7
+      "7 bottles of milk on the wall, " +
+      "7 bottles of milk.\n" +
+      "Take one down and pass it around, " +
+      "1 six-pack of milk on the wall.\n"
     else
       "#{number} bottles of milk on the wall, " +
       "#{number} bottles of milk.\n" +
