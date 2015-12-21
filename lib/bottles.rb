a3524f0ffdc8610bf7b3f6ee294bd6290a838f42
Katrina Owen <katrina.owen@gmail.com>
2015-12-21 10:16:55 -0700

    refactor to a sparse conditional

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,15 +1,14 @@
 class Bottles
   def verse(number)
     if number == 99
-      "99 bottles of milk on the wall, " +
-      "99 bottles of milk.\n" +
-      "Take one down and pass it around, " +
-      "98 bottles of milk on the wall.\n"
+      n = 99
     else
-      "3 bottles of milk on the wall, " +
-      "3 bottles of milk.\n" +
-      "Take one down and pass it around, " +
-      "2 bottles of milk on the wall.\n"
+      n = 3
     end
+
+    "#{n} bottles of milk on the wall, " +
+    "#{n} bottles of milk.\n" +
+    "Take one down and pass it around, " +
+    "#{n - 1} bottles of milk on the wall.\n"
   end
 end
