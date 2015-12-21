92f6523972d4dc45ff7db93fd110cd1018e0b9c4
Katrina Owen <katrina.owen@gmail.com>
2015-12-21 10:17:54 -0700

    replace conditional with string interpolation

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,14 +1,8 @@
 class Bottles
   def verse(number)
-    if number == 99
-      n = 99
-    else
-      n = 3
-    end
-
-    "#{n} bottles of milk on the wall, " +
-    "#{n} bottles of milk.\n" +
+    "#{number} bottles of milk on the wall, " +
+    "#{number} bottles of milk.\n" +
     "Take one down and pass it around, " +
-    "#{n - 1} bottles of milk on the wall.\n"
+    "#{number-1} bottles of milk on the wall.\n"
   end
 end
