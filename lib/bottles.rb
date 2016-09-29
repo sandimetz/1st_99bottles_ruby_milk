39a532205222faacc77fe1d97c6a6ceee3be0e72
Katrina Owen <kytrinyx@github.com>
2016-09-29 12:12:34 -0600

    Create and use instance of next bottle number

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -9,11 +9,12 @@ class Bottles
 
   def verse(number)
     bottle_number = BottleNumber.new(number)
+    next_bottle_number = BottleNumber.new(bottle_number.successor)
 
     "#{bottle_number.quantity.capitalize} #{bottle_number.container} of milk on the wall, " +
     "#{bottle_number.quantity} #{bottle_number.container} of milk.\n" +
     "#{bottle_number.action}, " +
-    "#{quantity(successor(number))} #{container(successor(number))} of milk on the wall.\n"
+    "#{next_bottle_number.quantity} #{next_bottle_number.container} of milk on the wall.\n"
   end
 
   def container(number)
