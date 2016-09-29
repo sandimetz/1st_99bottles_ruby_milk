15778ed674614c0d3c8494aea84a9797a2e1b46f
Katrina Owen <kytrinyx@github.com>
2016-09-29 12:11:17 -0600

    Use cached bottle number instance in verse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -10,9 +10,9 @@ class Bottles
   def verse(number)
     bottle_number = BottleNumber.new(number)
 
-    "#{quantity(number).capitalize} #{container(number)} of milk on the wall, " +
-    "#{quantity(number)} #{container(number)} of milk.\n" +
-    "#{action(number)}, " +
+    "#{bottle_number.quantity.capitalize} #{bottle_number.container} of milk on the wall, " +
+    "#{bottle_number.quantity} #{bottle_number.container} of milk.\n" +
+    "#{bottle_number.action}, " +
     "#{quantity(successor(number))} #{container(successor(number))} of milk on the wall.\n"
   end
 
